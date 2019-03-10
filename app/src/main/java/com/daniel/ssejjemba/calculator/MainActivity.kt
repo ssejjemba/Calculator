f1e2c571b86526ccf948b9a904c32982a3b621c4
Daniel <ssejjembadan@gmail.com>
2019-03-10 16:01:16 +0300

    Calculator logic completed

--- app/src/main/java/com/daniel/ssejjemba/calculator/MainActivity.kt
+++ app/src/main/java/com/daniel/ssejjemba/calculator/MainActivity.kt
@@ -4,22 +4,32 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buAC.setOnClickListener(View.OnClickListener {
+            isFirstOperation = true
+            isNewOperation = true
+            etResults.setText("0")
+            oldNumber = ""
+        })
     }
 
     protected fun btnNumberEvent(view: View){
-        val buSelected = view as Button
-        var buClickValue:String = etResults.text.toString()
-        if(isNewOperation==true){
+        if(isNewOperation){
             etResults.setText("")
         }
         isNewOperation = false
+        val buSelected = view as Button
+        var buClickValue:String = etResults.text.toString()
+
 
         when(buSelected.id){
             R.id.bu0 -> {
@@ -55,6 +65,7 @@ class MainActivity : AppCompatActivity() {
     var op = "*"
     var oldNumber = ""
     var isNewOperation = true
+    var isFirstOperation = true
 
     protected fun btnOperationEvent(view:View){
         val buSelected = view as Button
@@ -67,7 +78,39 @@ class MainActivity : AppCompatActivity() {
             R.id.buPercent -> op = "%"
         }
 
-        oldNumber = etResults.text.toString()
+        if(isFirstOperation) {
+            oldNumber = etResults.text.toString()
+        }
+        else {
+            oldNumber = resultOfOperation().toString()
+            etResults.setText(oldNumber)
+        }
         isNewOperation = true
+        isFirstOperation = false
+    }
+
+    private fun resultOfOperation(): Double{
+        var result: Double
+        val newNumber = etResults.text.toString().toDouble()
+        try{
+        result = when(op){
+            "*" -> oldNumber.toDouble() * newNumber
+            "-" -> oldNumber.toDouble() - newNumber
+            "+" -> oldNumber.toDouble() + newNumber
+            "%" -> oldNumber.toDouble() % newNumber
+            "/" -> oldNumber.toDouble() / newNumber
+            else -> 0.0
+        }}catch (ex:Exception){
+            result = 0.0
+            Toast.makeText(this, "You cannot Divide By Zero", Toast.LENGTH_LONG).show()
+        }
+        return result
+    }
+
+    protected fun btnEquals(view:View){
+        val finalNumber = resultOfOperation()
+        etResults.setText(finalNumber.toString())
+        isNewOperation = true
+        isFirstOperation = true
     }
 }
